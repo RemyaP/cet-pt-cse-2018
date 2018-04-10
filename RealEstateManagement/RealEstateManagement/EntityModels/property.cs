991a8866319e18b28dc58df6a6ebc1a61a9c4c2e
RemyaP <remya.pankajakshan@gmail.com>
2018-04-11 00:20:45 +0530

    property area

--- RealEstateManagement/RealEstateManagement/EntityModels/property.cs
+++ RealEstateManagement/RealEstateManagement/EntityModels/property.cs
@@ -20,6 +20,8 @@ namespace RealEstateManagement.Models
         [Key]
         public int property_id { get; set; }
 
+        public double? area { get; set; }
+
         public double? latitude { get; set; }
 
         public double? longitude { get; set; }
