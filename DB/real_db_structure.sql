989eb7d563635bac0dcc911c355c3162e15afa89
RemyaP <remya.pankajakshan@gmail.com>
2018-04-21 10:43:52 +0530

    Structure and values for master tables

--- DB/real_db_structure.sql
+++ DB/real_db_structure.sql
@@ -1,207 +1,314 @@
--- MySQL Workbench Synchronization
--- Generated: 2018-03-24 15:21
--- Model: New Model
--- Version: 1.0
--- Project: Name of the project
--- Author: Remya
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: real_estate
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE SCHEMA IF NOT EXISTS `real_estate` DEFAULT CHARACTER SET utf8 ;
+--
+-- Table structure for table `bid`
+--
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`user` (
-  `user_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(20) NOT NULL,
-  `password` VARCHAR(20) NOT NULL,
-  `first_name` VARCHAR(45) NULL DEFAULT NULL,
-  `last_name` VARCHAR(45) NULL DEFAULT NULL,
-  `address` VARCHAR(45) NULL DEFAULT NULL,
-  `pan_no` VARCHAR(45) NULL DEFAULT NULL,
-  `mob_no` VARCHAR(45) NULL DEFAULT NULL,
-  `email` VARCHAR(45) NULL DEFAULT NULL,
-  `status_id` INT(11) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  INDEX `fk_user_status1_idx` (`status_id` ASC),
-  CONSTRAINT `fk_user_status1`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `real_estate`.`status` (`status_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DROP TABLE IF EXISTS `bid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bid` (
+  `bid_id` int(11) NOT NULL AUTO_INCREMENT,
+  `property_id` int(11) NOT NULL,
+  `buyer_id` int(11) NOT NULL,
+  `status` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`bid_id`),
+  KEY `fk_purchase_plot1_idx` (`property_id`),
+  KEY `fk_purchase_buyer1_idx` (`buyer_id`),
+  CONSTRAINT `fk_purchase_buyer1` FOREIGN KEY (`buyer_id`) REFERENCES `buyer` (`buyer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_purchase_plot1` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`seller` (
-  `seller_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `seller_type` INT(11) NOT NULL,
-  `user_id` INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`seller_id`),
-  INDEX `fk_seller_user_idx` (`user_id` ASC),
-  CONSTRAINT `fk_seller_user`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `real_estate`.`user` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Table structure for table `bid_price`
+--
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`buyer` (
-  `buyer_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `min_area` DOUBLE UNSIGNED NULL DEFAULT NULL,
-  `max_area` DOUBLE UNSIGNED NULL DEFAULT NULL,
-  `min_cost` INT(10) UNSIGNED NULL DEFAULT NULL,
-  `max_cost` INT(10) UNSIGNED NULL DEFAULT NULL,
-  `user_id` INT(10) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `bid_price`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bid_price` (
+  `bid_id` int(11) NOT NULL,
+  `plot_price` double DEFAULT NULL,
+  `apartment_price` double DEFAULT NULL,
+  PRIMARY KEY (`bid_id`),
+  KEY `fk_bid_price_bid1_idx` (`bid_id`),
+  CONSTRAINT `fk_bid_price_bid1` FOREIGN KEY (`bid_id`) REFERENCES `bid` (`bid_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `buyer`
+--
+
+DROP TABLE IF EXISTS `buyer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buyer` (
+  `buyer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `min_area` double unsigned DEFAULT NULL,
+  `max_area` double unsigned DEFAULT NULL,
+  `min_cost` int(10) unsigned DEFAULT NULL,
+  `max_cost` int(10) unsigned DEFAULT NULL,
+  `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`buyer_id`),
-  INDEX `fk_buyer_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_buyer_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `real_estate`.`user` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_buyer_user1_idx` (`user_id`),
+  CONSTRAINT `fk_buyer_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`property` (
-  `property_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `area` DOUBLE UNSIGNED NULL DEFAULT NULL,
-  `latitude` DOUBLE NULL DEFAULT NULL,
-  `longitude` DOUBLE NULL DEFAULT NULL,
-  `seller_id` INT(11) NOT NULL,
-  `category` INT(11) NOT NULL,
-  PRIMARY KEY (`property_id`),
-  INDEX `fk_plot_seller1_idx` (`seller_id` ASC),
-  INDEX `fk_plot_category1_idx` (`category` ASC),
-  CONSTRAINT `fk_plot_seller1`
-    FOREIGN KEY (`seller_id`)
-    REFERENCES `real_estate`.`seller` (`seller_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_plot_category1`
-    FOREIGN KEY (`category`)
-    REFERENCES `real_estate`.`category` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Table structure for table `category`
+--
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`bid` (
-  `bid_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `property_id` INT(11) NOT NULL,
-  `buyer_id` INT(11) NOT NULL,
-  `status` INT(10) UNSIGNED NULL DEFAULT NULL,
-  PRIMARY KEY (`bid_id`),
-  INDEX `fk_purchase_plot1_idx` (`property_id` ASC),
-  INDEX `fk_purchase_buyer1_idx` (`buyer_id` ASC),
-  CONSTRAINT `fk_purchase_plot1`
-    FOREIGN KEY (`property_id`)
-    REFERENCES `real_estate`.`property` (`property_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_purchase_buyer1`
-    FOREIGN KEY (`buyer_id`)
-    REFERENCES `real_estate`.`buyer` (`buyer_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`property_has_landmark` (
-  `property_id` INT(11) NOT NULL,
-  `landmark_id` INT(10) UNSIGNED NOT NULL,
-  INDEX `fk_landmark_plot_idx` (`property_id` ASC),
-  PRIMARY KEY (`property_id`, `landmark_id`),
-  CONSTRAINT `fk_landmark_plot1`
-    FOREIGN KEY (`property_id`)
-    REFERENCES `real_estate`.`property` (`property_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_landmark_railway1`
-    FOREIGN KEY (`landmark_id`)
-    REFERENCES `real_estate`.`landmark` (`landmark_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Dumping data for table `category`
+--
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`landmark` (
-  `landmark_id` INT(10) UNSIGNED NOT NULL,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  `latitude` DOUBLE NULL DEFAULT NULL,
-  `longitude` DOUBLE NULL DEFAULT NULL,
-  `landmark_type` INT(11) NOT NULL,
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Land'),(2,'House'),(3,'Flat');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `property_id` int(11) NOT NULL,
+  `image` blob NOT NULL,
+  KEY `fk_images_plot1_idx` (`property_id`),
+  CONSTRAINT `fk_images_plot1` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `landmark`
+--
+
+DROP TABLE IF EXISTS `landmark`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `landmark` (
+  `landmark_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `landmarktype` int(11) NOT NULL,
   PRIMARY KEY (`landmark_id`),
-  INDEX `fk_landmark_landmarktype1_idx` (`landmark_type` ASC),
-  CONSTRAINT `fk_landmark_landmarktype1`
-    FOREIGN KEY (`landmark_type`)
-    REFERENCES `real_estate`.`landmarktype` (`type_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_landmark_landmarktype1_idx` (`landmarktype`),
+  CONSTRAINT `fk_landmark_landmarktype1` FOREIGN KEY (`landmarktype`) REFERENCES `landmarktype` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`images` (
-  `property_id` INT(11) NOT NULL,
-  `image` BLOB NOT NULL,
-  INDEX `fk_images_plot1_idx` (`property_id` ASC),
-  CONSTRAINT `fk_images_plot1`
-    FOREIGN KEY (`property_id`)
-    REFERENCES `real_estate`.`property` (`property_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Table structure for table `landmarktype`
+--
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`status` (
-  `status_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `status` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`status_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DROP TABLE IF EXISTS `landmarktype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `landmarktype` (
+  `type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`landmarktype` (
-  `type_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `type_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`type_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Dumping data for table `landmarktype`
+--
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`category` (
-  `category_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`category_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+LOCK TABLES `landmarktype` WRITE;
+/*!40000 ALTER TABLE `landmarktype` DISABLE KEYS */;
+INSERT INTO `landmarktype` VALUES (1,'School'),(2,'Hospital'),(3,'Railway'),(4,'Church'),(5,'Temple'),(6,'Mosque');
+/*!40000 ALTER TABLE `landmarktype` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`min_price` (
-  `property_id` INT(11) NOT NULL,
-  `plot_price` DOUBLE NULL DEFAULT NULL,
-  `apartment_price` DOUBLE NULL DEFAULT NULL,
+--
+-- Table structure for table `min_price`
+--
+
+DROP TABLE IF EXISTS `min_price`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `min_price` (
+  `property_id` int(11) NOT NULL,
+  `plot_price` double DEFAULT NULL,
+  `apartment_price` double DEFAULT NULL,
   PRIMARY KEY (`property_id`),
-  CONSTRAINT `fk_min_price_property1`
-    FOREIGN KEY (`property_id`)
-    REFERENCES `real_estate`.`property` (`property_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  CONSTRAINT `fk_min_price_property1` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `real_estate`.`bid_price` (
-  `bid_id` INT(11) NOT NULL,
-  `plot_price` DOUBLE NULL DEFAULT NULL,
-  `apartment_price` DOUBLE NULL DEFAULT NULL,
-  PRIMARY KEY (`bid_id`),
-  INDEX `fk_bid_price_bid1_idx` (`bid_id` ASC),
-  CONSTRAINT `fk_bid_price_bid1`
-    FOREIGN KEY (`bid_id`)
-    REFERENCES `real_estate`.`bid` (`bid_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Table structure for table `property`
+--
 
+DROP TABLE IF EXISTS `property`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `property` (
+  `property_id` int(11) NOT NULL AUTO_INCREMENT,
+  `area` double unsigned DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `seller_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `category` int(11) NOT NULL,
+  PRIMARY KEY (`property_id`),
+  KEY `fk_plot_seller1_idx` (`seller_id`),
+  KEY `fk_property_propery_status1_idx` (`status`),
+  KEY `fk_property_category1_idx` (`category`),
+  CONSTRAINT `fk_plot_seller1` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`seller_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_property_category1` FOREIGN KEY (`category`) REFERENCES `category` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_property_propery_status1` FOREIGN KEY (`status`) REFERENCES `propery_status` (`status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Table structure for table `property_has_landmark`
+--
+
+DROP TABLE IF EXISTS `property_has_landmark`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `property_has_landmark` (
+  `property_property_id` int(11) NOT NULL,
+  `landmark_landmark_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`property_property_id`,`landmark_landmark_id`),
+  KEY `fk_property_has_landmark_property1_idx` (`property_property_id`),
+  KEY `fk_property_has_landmark_landmark1_idx` (`landmark_landmark_id`),
+  CONSTRAINT `fk_property_has_landmark_landmark1` FOREIGN KEY (`landmark_landmark_id`) REFERENCES `landmark` (`landmark_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_property_has_landmark_property1` FOREIGN KEY (`property_property_id`) REFERENCES `property` (`property_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `propery_status`
+--
+
+DROP TABLE IF EXISTS `propery_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `propery_status` (
+  `status_id` int(11) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`status_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `propery_status`
+--
+
+LOCK TABLES `propery_status` WRITE;
+/*!40000 ALTER TABLE `propery_status` DISABLE KEYS */;
+INSERT INTO `propery_status` VALUES (1,'Open'),(2,'Started'),(3,'Closed'),(4,'Sold');
+/*!40000 ALTER TABLE `propery_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seller`
+--
+
+DROP TABLE IF EXISTS `seller`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seller` (
+  `seller_id` int(11) NOT NULL AUTO_INCREMENT,
+  `seller_type` int(11) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`seller_id`),
+  KEY `fk_seller_user_idx` (`user_id`),
+  CONSTRAINT `fk_seller_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'Approved'),(2,'Blocked');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `pan_no` varchar(45) DEFAULT NULL,
+  `mob_no` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `status_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `fk_user_status1_idx` (`status_id`),
+  CONSTRAINT `fk_user_status1` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-21  0:26:50
